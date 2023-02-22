b37f45ee0dcda52f78d3bf8c52adb455e60b1b18
Polina Che <perhaybe@gmail.com>
2023-02-23 02:12:47 +0300

    feat: about project

--- src/components/AboutProject/AboutProject.jsx
+++ src/components/AboutProject/AboutProject.jsx
@@ -2,25 +2,31 @@ import './AboutProject.css';
 
 export default function AboutProject() {
   return (
-    <div className="about-project">
-      <h2 className="about-project__header">О проекте</h2>
-      <div className="about-project__steps">
-        <div className="about-project__step">
-          <h3>Дипломный проект включал 5 этапов</h3>
-          <p>Составление плана, работу над бэкендом, вёрстку, добавление функциональности и финальные доработки.</p>
+    <div className="proj">
+      <h2 className="proj__header">О проекте</h2>
+      <div className="proj__steps">
+        <div className="proj__step">
+          <h3 className="proj__step-header">Дипломный проект включал 5 этапов</h3>
+          <p className="proj__step-paragraph">
+            Составление плана, работу над бэкендом, вёрстку, добавление функциональности и финальные доработки.
+          </p>
         </div>
-        <div className="about-project__step">
-          <h3>На выполнение диплома ушло 5 недель</h3>
-          <p>У каждого этапа был мягкий и жёсткий дедлайн, которые нужно было соблюдать, чтобы успешно защититься.</p>
+        <div className="proj__step">
+          <h3 className="proj__step-header">На выполнение диплома ушло 5 недель</h3>
+          <p className="proj__step-paragraph">
+            У каждого этапа был мягкий и жёсткий дедлайн, которые нужно было соблюдать, чтобы успешно защититься.
+          </p>
         </div>
       </div>
-      <div>
-        <p>1 неделя</p>
-        <p>4 недели</p>
-      </div>
-      <div>
-        <p>Back-end</p>
-        <p>Front-end</p>
+      <div className="proj__web">
+        <div className="proj__back">
+          <p className="proj__duration proj__back-duration">1 неделя</p>
+          <p className="proj__web-paragraph">Back-end</p>
+        </div>
+        <div className="proj__front">
+          <p className="proj__duration proj__front-duration">4 недели</p>
+          <p className="proj__web-paragraph">Front-end</p>
+        </div>
       </div>
     </div>
   );
