70bc8362d7dfd4e2766986b7cf19eb4e52846abc
Polina Che <perhaybe@gmail.com>
2023-03-01 05:10:09 +0300

    feat: routes

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,15 +1,19 @@
+/* eslint-disable no-unused-vars */
+import { Route, Routes } from 'react-router-dom';
+
 import './App.css';
 
 import Header from '../Header/Header';
 import Main from '../Main/Main';
 import Movies from '../Movies/Movies';
-// import SavedMovies from '../SavedMovies/SavedMovies';
-// import Profile from '../Profile/Profile';
-// import Login from '../Login/Login';
-// import PageNotFound from '../NotFoundPage/PageNotFound';
-// import Register from '../Register/Register';
+import SavedMovies from '../SavedMovies/SavedMovies';
+import Profile from '../Profile/Profile';
+import Login from '../Login/Login';
+import PageNotFound from '../NotFoundPage/PageNotFound';
+import Register from '../Register/Register';
 import Footer from '../Footer/Footer';
 
+// tmp user
 const userLogIn = true;
 
 function App() {
@@ -17,17 +21,24 @@ function App() {
     <div className="app">
       <Header />
 
-      {userLogIn ? (
-        <Movies isSavedSection={false} />
-        // <SavedMovies isSavedSection />
-        // <Profile />
-        // <Login />
-        // <Register />
-        // <PageNotFound/>
+      <Routes>
+        <Route path="/" element={<Main />} />
 
+        {userLogIn ? (
+          <>
+            <Route path="/movies" element={<Movies isSavedSection={false} />} />
+            <Route path="/saved-movies" element={<SavedMovies isSavedSection />} />
+            <Route path="/profile" element={<Profile />} />
+            <Route path="/signin" element={<Login />} />
+            <Route path="/signup" element={<Register />} />
+          </>
       ) : (
         <Main />
       )}
+
+      <Route path="*" element={<PageNotFound/>} />
+
+      </Routes>
 
       <Footer />
     </div>
