f99b4a9ca1954c9a8e614a581883a4cf4bd818bf
Polina Che <perhaybe@gmail.com>
2023-03-05 01:17:45 +0300

    feat: search input validation

--- src/components/SearchForm/SearchForm.jsx
+++ src/components/SearchForm/SearchForm.jsx
@@ -1,23 +1,95 @@
+/* eslint-disable import/named */
+/* eslint-disable no-unused-vars */
+
+import { useState, useEffect } from 'react';
+import searchValidator from '../../utils/searchValidator';
 import './SearchForm.css';
 import FilterCheckbox from '../FilterCheckbox/FilterCheckbox';
 
 export default function SearchForm() {
+  const [searchInputValue, setSearchInputValue] = useState({ searchinput: '' });
+  const [inputsValidation, setInputsValidation] = useState({ searchinput: { isValid: true, errorText: '' } });
+  const isFormInvalid = inputsValidation.searchinput.isValid;
+
+  function validateSearchInput(value) {
+    const validationResult = searchValidator(value);
+    setInputsValidation((pervState) => ({
+      ...pervState,
+      searchinput: { isValid: validationResult.isValid, errorText: validationResult.errorText },
+    }));
+  }
+
+  function setDefaultInputsValidation() {
+    setInputsValidation((prevValue) => ({
+      ...prevValue,
+      searchinput: { isValid: true, errorText: '' },
+    }));
+  }
+
+  function handleSearch(evt) {
+    evt.preventDefault();
+    validateSearchInput(searchInputValue.searchinput);
+    console.log('data ok, send next')
+  }
+
+  function handleChange(evt) {
+    const { name, value } = evt.target;
+    setSearchInputValue((prevState) => ({
+      ...prevState,
+      [name]: value,
+    }));
+
+    validateSearchInput(value);
+  }
+
+  function handleFocus(evt) {
+    const { name, value } = evt.target;
+    validateSearchInput(value);
+    setSearchInputValue((prevState) => ({
+      ...prevState,
+      [name]: value,
+    }));
+  }
+
+  function handleBlur() {
+    setDefaultInputsValidation();
+  }
+
+  useEffect(() => {
+    setSearchInputValue((prevValue) => ({
+      ...prevValue,
+      searchinput: '',
+    }));
+
+    setDefaultInputsValidation();
+  }, []);
+
   return (
     <div className="search-form">
-      <form className="search-form__inputs">
+      <form className="search-form__inputs" noValidate>
         <input
           className="search-form__input"
           type="search"
-          id="search-form-input"
-          name="search-form-input"
+          id="searchinput"
+          name="searchinput"
           placeholder="Фильм"
           required
+          value={searchInputValue.searchinput}
+          onChange={handleChange}
+          onFocus={handleFocus}
+          onBlur={handleBlur}
         />
-        <button className="search-form__submit" type="submit" name="search-movie">
+        <button
+          className="search-form__submit"
+          type="submit"
+          name="search-movie"
+          disabled={!isFormInvalid}
+          onClick={handleSearch}
+        >
           Найти
         </button>
       </form>
-      <span className="search-form__input-error">error text</span>
+      <span className="search-form__input-error">{!isFormInvalid ? inputsValidation.searchinput.errorText : ''}</span>
       <FilterCheckbox />
     </div>
   );
