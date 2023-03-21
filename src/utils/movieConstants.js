ccbec01c4d6791a1cec32c8c1103c898169e34b4
Polina Che <perhaybe@gmail.com>
2023-03-21 17:07:01 +0300

    fix: n-Row of movies for small window

--- src/utils/movieConstants.js
+++ src/utils/movieConstants.js
@@ -1,7 +1,7 @@
 const MOVIE_CARD_PARAMS = {
   large: { movieHeight: 218, movieGap: 32, movieRow: 1 },
   medium: { movieHeight: 257, movieGap: 36, movieRow: 1 },
-  small: { movieHeight: 235, movieGap: 20, movieRow: 1 },
+  small: { movieHeight: 235, movieGap: 20, movieRow: 5 },
 };
 
 export default MOVIE_CARD_PARAMS
